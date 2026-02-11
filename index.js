9118b530d6ad4a41c2fefb991e9c26047e7ed0a8
bharath <bharath.balasubramaniam@impelox.com>
2026-02-11 18:03:03 +0530

    inital vim commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/",(req,res)=>{
+  return res.send("hello world")
+})
+
+const PORT=3000;
+app.listen(PORT,()=>{
+  console.log('server is listering')
+})
